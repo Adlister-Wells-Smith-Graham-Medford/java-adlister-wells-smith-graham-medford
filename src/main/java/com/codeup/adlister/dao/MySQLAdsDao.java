4e58ac3291716c41c6aeee354e5ec4b42fff6f33
Johnny Wells <johnnyw01github@gmail.com>
2023-03-21 13:47:09 -0500

    Commiting.

--- src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
@@ -160,7 +160,6 @@ public class MySQLAdsDao implements Ads {
         }
     }
 
-
     private Ad extractAd(ResultSet rs) throws SQLException {
         return new Ad(
                 rs.getLong("id"),
